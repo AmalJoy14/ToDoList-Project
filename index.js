374c543883340393c946b14d8759000dcf58cf22
AmalJoy14 <amaljoy246@gmail.com>
2024-04-14 21:39:29 +0530

    updated

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(express.static("public"));
+app.use(express.static("views"));
 
 app.set("view engine", "ejs");
 
